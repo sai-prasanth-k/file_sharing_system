e3894dd060926a92011d1fdf05b80feb2ff0bc1d
Sai Prasanth K <saiprasanth5293@gmail.com>
2024-11-22 16:39:32 +0530

    .

--- file_sharing/file_sharing/settings.py
+++ file_sharing/file_sharing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-**sfg&%lc-8i9r!rsd2gnn0&#4p(xeisu!ltq2e=4sbxmmbfk@'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
